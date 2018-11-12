53877e3daca893c0a3082e57dbc74fd14257e3f3
anamesh3arefni <ahmed.magdy.dev@gmail.com>
2018-11-12 20:14:06 +0200

    arrow angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {DashboardData} from './data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'arrow';
+export class AppComponent implements OnInit {
+  private DashboardData:Array<any> = DashboardData;
+  public ngOnInit():void {
+
+      console.log(this.DashboardData)
+    }
+
 }
