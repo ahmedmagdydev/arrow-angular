d48f3426d617e5bc0253856f47a7754d0fdf0f68
anamesh3arefni <ahmed.magdy.dev@gmail.com>
2018-11-15 18:34:59 +0200

    public data

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/arrow-angular'));
+app.use(express.static(__dirname + '/dist/arrow'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/arrow-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/arrow/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
