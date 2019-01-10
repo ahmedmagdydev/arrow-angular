11cf707b1d6ee87d3a0c7fdb6e7f5ebeab8294fa
anamesh3arefni <ahmed.magdy.dev@gmail.com>
2019-01-10 23:25:00 +0200

    sa

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/arrow'));
 
+// enable CORS 
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
+
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/arrow/index.html'));
